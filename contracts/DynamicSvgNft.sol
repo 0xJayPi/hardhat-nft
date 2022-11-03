a4d0c892896e3f80cfee1c3a1b6e6345707c2e68
0xJayPi <jpcampaya@gmail.com>
2022-11-03 11:36:57 +0100

    DynamicSvgNft.sol code completed

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
@@ -9,9 +10,20 @@ contract DynamicSvgNft is ERC721 {
     string private i_lowImageURI;
     string private i_highImageURI;
     string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValue;
 
-    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+        i_lowImageURI = svgToImageURI(lowSvg);
+        i_highImageURI = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
@@ -19,8 +31,43 @@ contract DynamicSvgNft is ERC721 {
         return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 
-    function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValue[s_tokenCounter] = highValue;
         s_tokenCounter++;
+        _safeMint(msg.sender, s_tokenCounter);
+        emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "URI Query for nonexistent token"); // function from the ERC721 contract
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = i_lowImageURI;
+        if (price >= s_tokenIdToHighValue[tokenId]) {
+            imageURI = i_highImageURI;
+        }
+
+        // return a JSON string encoded in base64
+        return
+            string(
+                abi.encodePacked(
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"An NFT that changes values based on Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}"'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
