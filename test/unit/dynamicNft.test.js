5a717aaed168a1fca991cbab1e62b9397ca4e4fb
0xJayPi <jpcampaya@gmail.com>
2022-11-04 10:58:42 +0100

    dynamicNft.test.js initiated

---
+++ test/unit/dynamicNft.test.js
@@ -0,0 +1,5 @@
+const { assert } = require("chai")
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name) ? describe.skip : describe()
